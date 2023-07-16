c890fa6e8b77cb84c2f6927cd8b9db0a95adef0a
HeukKell <falcfmpcv@gmail.com>
2023-07-16 20:08:26 +0900

    Init Project
    프로젝트 초기화

---
+++ GameServer/ServerCore/CorePch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+/*
+	DummyClient 나 GameServer 프로젝트에서 참조 가능한 헤더파일
+*/
+
+
+#include "Types.h"
+
+#include  <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+void HelloWorld();
---
+++ GameServer/ServerCore/pch.h
@@ -0,0 +1,14 @@
+﻿// ServerCore 프로젝트내에서 사용할것들을 모아놓은곳
+
+/**
+#ifndef PCH_H
+#define PCH_H
+#endif //PCH_H
+*/
+#pragma once	// 위 ifndef 을 #pragma once 로 치환
+
+// 여기에 미리 컴파일하려는 헤더 추가
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+
+#include "CorePch.h"
+
