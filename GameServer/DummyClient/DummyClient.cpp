cdc845f7f7904ea2ea74ec1a19a1e3dee5242def
HeukKell <falcfmpcv@gmail.com>
2024-02-22 13:03:39 +0900

    std::tread Test

--- GameServer/DummyClient/DummyClient.cpp
+++ GameServer/DummyClient/DummyClient.cpp
@@ -1,8 +1,43 @@
-﻿#include "pch.h"
-#include <iostream>
+﻿#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <thread>
 
-int main()
+
+
+using namespace std;
+using int32 = __int32;
+using int64 = __int64;
+
+/*
+	thread 마다 자신만의 공간을 따로 갖게 된다.
+	thread 마다 고유하게 들고 있어야만 하는 정보들을 넣어 활용할 수 있다.
+*/
+thread_local int32 LThreadId = 0;
+
+void ThreadMain(int32 threadId)
 {
-    HelloWorld();
+	LThreadId = threadId;
+
+	while (true)
+	{
+		cout << "Thread [" << threadId << "]" << endl;
+		this_thread::sleep_for(1s);
+	}
 }
 
+void main()
+{
+	vector<thread> threads;
+
+	for (int32 i = 0; i < 10; i++)
+	{
+		int32 threadId = i + 1;
+		threads.push_back(thread(ThreadMain, threadId));
+	}
+
+	for (thread& threadElem : threads)
+	{
+		threadElem.join(); // thread 합류
+	}
+}
